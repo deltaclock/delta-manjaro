a08acc47daadde7210d8ec3748a15a6b3e190412
delta <delta@beast>
2019-06-04 19:09:20 +0300

    more tools

--- setup.sh
+++ setup.sh
@@ -12,6 +12,9 @@ git clone https://github.com/deltaclock/dirbuster-lists.git
 git clone https://github.com/danielmiessler/SecLists.git
 wget https://raw.githubusercontent.com/rebootuser/LinEnum/master/LinEnum.sh && mv LinEnum.sh linenum.sh
 wget https://gist.githubusercontent.com/deltaclock/ff1d639f2406a14756c57f2399e0ea17/raw/db4aa8d2d3b3d934f820916bfc68def49c013865/sh.perl
+wget https://github.com/DominicBreuker/pspy/releases/download/v1.0.0/pspy32
+wget https://github.com/DominicBreuker/pspy/releases/download/v1.0.0/pspy64
+
 
 python -m pip install --user wfuzz
 
@@ -22,8 +25,12 @@ cp -r bash/ ~/.config/
 
 go get github.com/OJ/gobuster && go install github.com/OJ/gobuster
 
+# VMS
 cd ~/Downloads
 wget http://releases.ubuntu.com/19.04/ubuntu-19.04-live-server-amd64.iso
 wget https://cdimage.kali.org/kali-2019.2/kali-linux-xfce-2019.2-amd64.iso
 wget https://az792536.vo.msecnd.net/vms/VMBuild_20180102/VirtualBox/IE11/IE11.Win7.VirtualBox.zip
 wget https://az792536.vo.msecnd.net/vms/VMBuild_20190311/VirtualBox/MSEdge/MSEdge.Win10.VirtualBox.zip
+unzip IE11.Win7.VirtualBox.zip && unzip MSEdge.Win10.VirtualBox.zip
+rm IE11.Win7.VirtualBox.zip MSEdge.Win10.VirtualBox.zip
+mkdir ~/Desktop/vm_shares/{kali,pwn,win7,win10}
