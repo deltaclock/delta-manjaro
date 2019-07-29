af8d7b910b584bcde3ef03f1e52dc4ff4f9dcf72
Nick <24370162+deltaclock@users.noreply.github.com>
2019-07-29 14:46:50 +0000

    Create smart_script.sh

---
+++ bash/scripts/smart_script.sh
@@ -0,0 +1,56 @@
+# Execute "script" command just once
+smart_script(){
+    # if there's no SCRIPT_LOG_FILE exported yet
+    if [ -z "$SCRIPT_LOG_FILE" ]; then
+        # make folder paths
+        logdirparent=~/Terminal_typescripts
+        logdirraw=raw/$(date +%F)
+        logdir=$logdirparent/$logdirraw
+        logfile=$logdir/$(date +%F_%T).$$.rawlog
+
+        # if no folder exist - make one
+        if [ ! -d $logdir ]; then
+            mkdir -p $logdir
+        fi
+
+        export SCRIPT_LOG_FILE=$logfile
+        export SCRIPT_LOG_PARENT_FOLDER=$logdirparent
+
+        # quiet output if no args are passed
+        if [ ! -z "$1" ]; then
+            script -f $logfile
+        else
+            script -f -q $logfile
+        fi
+
+        exit
+    fi
+}
+
+# Start logging into new file
+alias startnewlog='unset SCRIPT_LOG_FILE && smart_script -v'
+
+# Manually saves current log file: $ savelog logname
+savelog(){
+    # make folder path
+    manualdir=$SCRIPT_LOG_PARENT_FOLDER/manual
+    # if no folder exists - make one
+    if [ ! -d $manualdir ]; then
+        mkdir -p $manualdir
+    fi
+    # make log name
+    logname=${SCRIPT_LOG_FILE##*/}
+    logname=${logname%.*}
+    # add user logname if passed as argument
+    if [ ! -z $1 ]; then
+        logname=$logname'_'$1
+    fi
+    # make filepaths
+    txtfile=$manualdir/$logname'.txt'
+    rawfile=$manualdir/$logname'.rawlog'
+    # make .rawlog readable and save it to .txt file
+    cat $SCRIPT_LOG_FILE | perl -pe 's/\e([^\[\]]|\[.*?[a-zA-Z]|\].*?\a)//g' | col -b > $txtfile
+    # copy corresponding .rawfile
+    cp $SCRIPT_LOG_FILE $rawfile
+    printf 'Saved logs:\n    '$txtfile'\n    '$rawfile'\n'
+}
